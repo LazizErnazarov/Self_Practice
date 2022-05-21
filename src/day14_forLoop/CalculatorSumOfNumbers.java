b2a03132f497965ffd8ac7792cfa7e1d306c8029
lazizern <laziz.ibodullaev.82@gmail.com>
2022-05-21 17:11:35 -0400

    updated

---
+++ src/day14_forLoop/CalculatorSumOfNumbers.java
@@ -0,0 +1,28 @@
+package day14_forLoop;
+
+import java.util.Scanner;
+
+public class CalculatorSumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the last number: ");
+        int lastNumber = input.nextInt();
+        int result = 0;
+
+        for(int i = 1; i<=lastNumber; i++){
+            result +=i;
+        }
+        System.out.println("result = " + result);
+
+    }
+}
+/*Write a program that can calculate the sum of all numbers between 1 to any given number.
+            ex:
+                    input: 100
+                    output: 5050
+
+                    input: 50
+                    output: 1275
+
+ */
